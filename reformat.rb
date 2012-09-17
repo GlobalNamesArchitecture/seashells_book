bddf900dd3ea35ae15879d8bc19f818be97c4f90
Dmitry Mozzherin <dmozzherin@gmail.com>
2012-09-17 16:37:44 -0400

    seem to work??

--- reformat.rb
+++ reformat.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+require 'yaml'
 
 class Reader
   attr_reader :file
@@ -15,20 +16,58 @@ class Reader
 
   def find_offsets
     count = 0
-    require 'ruby-debug'; debugger
     @pages.each_with_index do |page, i|
+      @cursor = 0
       page_name = "americanseashell_%.4d.txt" % i
-      f = open(File.join(File.dirname(__FILE__), 'pages', page_name), "w:utf-8") 
-      f.write(page.join(""))
-      f.close
+      page_processed = []
+      name_string = ''
       page.each do |l|
+        processed_line = ''
         no_start_tag = no_end_tag = false
         until no_start_tag && no_end_tag
-          l.index(@start_tag)
+          if @between_tags
+            match = l.index(@end_tag)
+            if match
+              @cursor += match
+              @offsets.last[:end_tag] = @cursor
+              @offsets.last[:end_page] = page_name
+              @offsets.last[:name_string] << l[0...match]
+              processed_line << l[0...match]
+              l = l[(match + @end_tag.size)..-1]
+              @between_tags = false
+            else
+              @offsets.last[:name_string] << l[0..-1]
+              @cursor += l.size
+              no_end_tag = true
+              no_start_tag = true
+              processed_line << l
+            end
+          else
+            match = l.index(@start_tag)
+            if match
+              @cursor += match
+              @offsets << {:start_tag => @cursor, :name_string => '', :start_page => page_name}
+              processed_line << l[0...match]
+              l = l[(match + @start_tag.size)..-1]
+              @between_tags = true
+            else
+              @cursor += l.size
+              no_start_tag = true
+              no_end_tag = true
+              processed_line << l
+            end
+          end
         end
-        @cursor += l.size
+        # @cursor += processed_line.size
+        page_processed << processed_line
       end
+      f = open(File.join(File.dirname(__FILE__), 'pages', page_name), "w:utf-8") 
+      f.write(page_processed.join(""))
+      f.close
     end
+    offsets = open('offsets.yml','w')
+    offsets.write(YAML.dump(@offsets))
+    offsets.close
   end
 end
 
